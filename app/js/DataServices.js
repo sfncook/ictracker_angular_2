b7611d24d26c704eae707c6d412e3095b4eb43da
Shawn Cook <cooksha@yahoo-inc.com>
2016-02-15 08:40:29 -0800

    Remove InitDefaultDatabase

--- app/js/DataServices.js
+++ app/js/DataServices.js
@@ -23,39 +23,6 @@ angular.module('DataServices', ['ParseServices', 'AdapterServices'])
         }
     }])
 
-    .factory('InitDefaultDatabase', [function () {
-        return function () {
-            if(ENABLE_SERVER_COMM && typeof Parse!='undefined') {
-                Parse.initialize("rGT3rpOCdLiXBniennYMpIr77IzzDAlTmGHwy1fO", "L0Brh9CVpryQ2yTIezbjLrEdBOfoVlbIMmtgUniJ");
-            }
-        }
-    }])
-
-    //.factory('InitDbForDepartment', ['ParseQuery', 'ConvertParseObject', 'InitDefaultDatabase', function (ParseQuery, ConvertParseObject, InitDefaultDatabase) {
-    //    return function (department) {
-    //        console.log("InitDbForDepartment department:"+department.app_key+", "+department.js_key);
-    //        Parse.initialize(department.app_key, department.js_key);
-    //    }
-    //}])
-    //
-    //.factory('InitDbForDepartmentId', ['ParseQuery', 'ConvertParseObject', 'InitDefaultDatabase', 'InitDbForDepartment', function (ParseQuery, ConvertParseObject, InitDefaultDatabase, InitDbForDepartment) {
-    //    return function (department_id) {
-    //        console.log("InitDbForDepartmentId department_id:"+department_id);
-    //        InitDefaultDatabase();
-    //        var queryDepartment = new Parse.Query(Parse.Object.extend('Department'));
-    //        queryDepartment.equalTo("objectId", department_id);
-    //        return queryDepartment.first({
-    //            success: function(department) {
-    //                ConvertParseObject(department, DEPARTMENT_DEF);
-    //                InitDbForDepartment(department);
-    //            },
-    //            error: function() {
-    //                console.log('Failed to find department_id:'+department_id+', with error code: ' + error.message);
-    //            }
-    //        });
-    //    }
-    //}])
-
     .factory('InitDataServices', function (AdapterStore) {
         return function () {
             return AdapterStore.init();
