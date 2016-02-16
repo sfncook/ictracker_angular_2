9ebe641fe96e9f3484703609c0a53cbbb91e894f
Shawn Cook <cooksha@yahoo-inc.com>
2016-02-15 21:08:40 -0800

    Reformat code DataServies

--- app/js/DataServices.js
+++ app/js/DataServices.js
@@ -14,16 +14,16 @@ angular.module('DataServices', ['ParseAdapter', 'StaticAdapter'])
       init: function () {
         var adapter_id_str = getHttpRequestByName('adapter');
 
-        if(adapter_id_str=="") {
+        if (adapter_id_str == "") {
           // Check for misspelling of work adapter
           adapter_id_str = getHttpRequestByName('adaptor');
-          if(adapter_id_str=="") {
+          if (adapter_id_str == "") {
             console.log("Missing required 'adapter' parameter. Using default 'parse' adapter.");
             this.adapter = ParseAdapter;
           }
         }
 
-        if(adapter_id_str!="") {
+        if (adapter_id_str != "") {
           if (ParseAdapter.adapter_id_str == adapter_id_str) {
             this.adapter = ParseAdapter;
           } else if (StaticAdapter.adapter_id_str == adapter_id_str) {
@@ -33,17 +33,25 @@ angular.module('DataServices', ['ParseAdapter', 'StaticAdapter'])
           }
         }
 
-        if(this.adapter.init) {
+        if (this.adapter.init) {
           return this.adapter.init();
         } else {
           return null;
         }
       },
       adapter: {},
-      isLoggedIn:         function()              {return this.adapter.isLoggedIn();},
-      SaveIncident:       function(incident)      {return this.adapter.SaveIncident(incident);},
-      SaveSector:         function(sector)        {return this.adapter.SaveSector(sector);},
-      SaveReportAction:   function(sector, text)  {return this.adapter.SaveReportAction(this.incident, sector, text);}
+      isLoggedIn: function () {
+        return this.adapter.isLoggedIn();
+      },
+      SaveIncident: function (incident) {
+        return this.adapter.SaveIncident(incident);
+      },
+      SaveSector: function (sector) {
+        return this.adapter.SaveSector(sector);
+      },
+      SaveReportAction: function (sector, text) {
+        return this.adapter.SaveReportAction(this.incident, sector, text);
+      }
     };
   })
 
@@ -58,10 +66,9 @@ angular.module('DataServices', ['ParseAdapter', 'StaticAdapter'])
   }])
 
 
-
-/*
- * Adaptor Services
- */
+  /*
+   * Adaptor Services
+   */
 
   .factory('LoadIncident', function (DataStore) {
     return function (incidentObjectId) {
