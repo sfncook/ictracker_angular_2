e2a508668f10db5c9d8e4ddac2386f2c0f7b326d
Shawn Cook <cooksha@yahoo-inc.com>
2016-02-27 18:40:23 -0800

    Action

--- app/js/models/Action.js
+++ app/js/models/Action.js
@@ -1,9 +1,15 @@
+// Default values
+var DEFAULT_category = '';
+var DEFAULT_incidentType = [];
+var DEFAULT_isWarning = false;
+var DEFAULT_name = '';
+
 function Action(category,     // string
                 incidentType, // string[]
                 isWarning,    // bool
                 name) {       // string
-  this.category = category;
-  this.incidentType = incidentType;
-  this.isWarning = isWarning;
-  this.name = name;
+  this.category = typeof category !== 'undefined' ? category : DEFAULT_category;
+  this.incidentType = typeof incidentType !== 'undefined' ? incidentType : DEFAULT_incidentType;
+  this.isWarning = typeof isWarning !== 'undefined' ? isWarning : DEFAULT_isWarning;
+  this.name = typeof name !== 'undefined' ? name : DEFAULT_name;
 }
